825732ad5c6f3701eb6e6e3df0bb55acd11a51e2
leekangwoon <deneb1593@gmail.com>
2022-03-21 23:24:17 +0900

    제거 코드 수정

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // ES 5이하 문법으로 진행
-
 (function(){
     
     var input, hashtagArray, container, t;
 
-    input = document.querySelector('#hashtags');
+    input = document.querySelector('#hashtag');
     container = document.querySelector('.tag-container');
     hashtagArray = [];
 
@@ -20,13 +19,17 @@
             input.value = '';
         
             var deleteTags = document.querySelectorAll('.tag');
-        
-            for(let i = 0; i < deleteTags.length; i++) {
-                deleteTags[i].addEventListener('click', function(e) {
-                    container.removeChild(deleteTags[i]);
-                });
-            }
-        }
 
+            for (var i = 0; i < deleteTags.length; i++) {
+                deleteTags[i].addEventListener('click', deleteTagHandler);
+            }     
+
+        }
     });
+
+    function deleteTagHandler(e) {
+        var parent = e.target.parentElement;
+        parent.removeChild(e.target);
+    }
+
 }());
